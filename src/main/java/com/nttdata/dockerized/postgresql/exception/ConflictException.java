c5a3b3cb13c201345f5779e20046df46e3324078
Carlos D. Adrianzen Adanaque <carlos.adrianzen27972@gmail.com>
2025-09-09 19:38:09 -0500

    refactor: eliminar DetallePedido y excepciones obsoletas

--- src/main/java/com/nttdata/dockerized/postgresql/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.dockerized.postgresql.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/nttdata/dockerized/postgresql/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.dockerized.postgresql.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
