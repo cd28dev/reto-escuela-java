6c0cefb8b67dc86740f45c9389c0683671d298e6
Carlos D. Adrianzen Adanaque <carlos.adrianzen27972@gmail.com>
2025-09-11 19:45:42 -0500

    feat(composition-order): agregar clientes Feign para User, Pedido y Product

--- composition-order/src/main/java/cd/dev/compositionorder/feign/UserServiceClient.java
+++ composition-order/src/main/java/cd/dev/compositionorder/feign/UserServiceClient.java
@@ -1,14 +1,18 @@
 package cd.dev.compositionorder.feign;
 
 import cd.dev.compositionorder.dto.external.UserResponseDto;
-import cd.dev.compositionorder.model.dto.external.UserResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${services.user-service.url}")
+import java.util.List;
+
+@FeignClient(name = "user-ms", url = "${microservices.user-ms.url}")
 public interface UserServiceClient {
 
     @GetMapping("/api/users/{id}")
-    UserResponseDto findById(@PathVariable("id") Long id);
+    UserResponseDto getUserById(@PathVariable Long id);
+
+    @GetMapping("/api/users")
+    List<UserResponseDto> getAllUsers();
 }
